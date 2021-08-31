987bcca6328e26d882530a6dd5f5df16b657e7e4
Whit Stroup <49410964+whitstroup@users.noreply.github.com>
2021-08-31 13:37:21 -0500

    Update ChallengesSet01.cs

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -19,41 +19,33 @@ namespace ChallengesWithTestsMark8
         public double Subtract(double minuend, double subtrahend)
         {
 
-            return minuend - subtrahend;
+          throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
-            return number1 + number2;
+           throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            return number1 < number2 ? number1 : number2;
+           throw new NotImplementedException();
 
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            return factor1 * factor2;
+            throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            if(nameOfPerson != "")
-            {
-            return $"Hello, {nameOfPerson}!";
-
-            }
-            else
-            {
-                return "Hello!";
-            }
+     throw new NotImplementedException();
         }
 
         public string GetHey()
         {
-            return "HEY!";
+            throw new NotImplementedException();
         }
     }
 }
