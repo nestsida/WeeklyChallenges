214ce39709d0eb50d27bb6c6e1f32716559bb621
sarah sida <nestsida@gmail.com>
2021-10-23 21:35:32 -0400

    initial commit

---
+++ ChallengesSet02.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet02
+    {
+        public bool CharacterIsALetter(char c)
+        {
+            return Char.IsLetter(c);
+            
+        }
+
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+            int count = 0;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                count++;
+            }
+            if (count % 2 == 0) 
+            {
+                return true;
+            }
+             else
+            {
+                return false;
+            }
+           
+           
+
+        }
+
+        public bool IsNumberEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsNumberOdd(int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }   
+               return numbers.Min() + numbers.Max();
+            
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+                return str1.Length;
+        }
+
+        public int Sum(int[] numbers)
+        {
+            return numbers == null ? 0 : numbers.Sum();
+            
+        }
+
+        public int SumEvens(int[] numbers)
+        {
+            return numbers == null ? 0 : numbers.Where(x => x % 2 == 0).Sum();
+        }
+
+        public bool IsSumOdd(List<int> numbers)
+        {
+            if (numbers == null)
+            {
+                return false;
+            }
+            int sum = numbers.Sum();
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+                return number / 2;
+        }
+    }
+}
